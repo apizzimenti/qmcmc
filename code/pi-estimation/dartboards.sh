b5dbaeac0a11820eaf3e80f2e084f70634c2ec3a
apizzimenti <apizzime@gmu.edu>
2023-10-01 19:20:11 -0400

    reduce # of colors

--- code/pi-estimation/dartboards.sh
+++ code/pi-estimation/dartboards.sh
@@ -8,6 +8,6 @@ fi
 
 # Convert all the images to gifs.
 cd output/figures/
-# convert -delay 8 -loop 0 -deconstruct dartboards/*.jpg _dartboards.gif
+# convert -delay 10 -loop 0 -deconstruct dartboards/*.jpg _dartboards.gif
 gifsicle --resize 800x_ --colors 16 _dartboards.gif > dartboards.gif
 
