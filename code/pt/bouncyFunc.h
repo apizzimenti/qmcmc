104b7ff3ddd7d81336d4d68881c93f82281d213c
Raghavendra Guggilam <69510186+raghugg@users.noreply.github.com>
2024-10-02 09:23:21 -0400

    Create bouncyFunc.h

---
+++ code/pt/bouncyFunc.h
@@ -0,0 +1,8 @@
+#ifndef BOUNCYFUNC_H
+#define BOUNCYFUNC_H
+
+double bouncyFunctionCost(double x, double imstupidanddontdoanything);
+double calculate_expression(double x, double dummy);
+double update(double x);
+
+#endif
